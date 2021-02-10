f53c39db3822a89b3f3cdb5c4e3f6dc1799ff857
Mila Savic <mila.savic@elfak.rs>
2021-02-10 16:58:37 +0100

    forme za logovanje, registrovanje...

---
+++ Frontend/beautysaloon/src/data/errorsCode.js
@@ -0,0 +1,9 @@
+export const ERRORS = [
+    "E-mail adresa je zauzeta.",
+    "Pogrešna stara lozinka.",
+    "Pogrešna e-mail adresa.",
+    "Pogrešna lozinka.",
+    "Korisničko ime je zauzeto.",
+    "Sifra je prekratka (najmanje 6 karaktera).",
+    "Nevalidna e-mail adresa."
+];
